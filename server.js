c0fe50fb777d25ceee9dc2f6622d6791cba94d8f
Dan Kaplun <dbkaplun@gmail.com>
2016-10-01 10:18:25 -0700

    feat(nlp): port to retext

--- server.js
+++ server.js
@@ -2,11 +2,18 @@
 
 const express = require('express');
 const bodyParser = require('body-parser');
-const nlp = require('nlp_compromise');
+const retext = require('retext');
+const retextPOS = require('retext-pos');
 
 let api = express.Router();
 api.post('/parse', (req, res) => {
-  res.send(nlp.text(req.body.text).terms());
+  retext()
+    .use(retextPOS) // this value is modified by the next .use() so we can't store this in a variable
+    .use(() => cst => {
+      res.send(cst);
+      console.log(`Parsed ${req.body.text.length} chars`);
+    })
+    .process(req.body.text);
 });
 
 let server = express();
